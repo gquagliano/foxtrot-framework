f728f20ad15e0fa8ca8629fa6b5b6101739fe47d
Gabriel Quagliano <gabriel.quagliano@gmail.com>
2020-06-28 14:31:46 -0300

    Compilación del framework.

--- scripts/construir-framework.php
+++ scripts/construir-framework.php
@@ -7,10 +7,55 @@
  */
 
 //Script de PRUEBA para construir y compilar los archivos del framework y el editor
-//TODO Deben exportarse todos los símbolos para poder utilizar --compilation_level ADVANCED
+
+//TODO Deben exportarse todos los símbolos para poder utilizar --compilation_level ADVANCED con Closure
+
+chdir(__DIR__);
 
 define('_closure','java -jar closure-compiler-v20200517.jar');
+define('_fuente',__DIR__.'/../fuente/');
+define('_desarrollo',__DIR__.'/../desarrollo/');
 
-//JS cliente (framework + componentes)
+include(__DIR__.'/funciones.php');
 
-exec(_closure.' --js_output_file "'.escapeshellarg(__DIR__.'/../cliente/foxtrot.min.js').'" "'.escapeshellarg(realpath(__DIR__.'/../cliente').'/**.js').'" "!foxtrot.min.js"');
+////php, html, css y otros recursos (se copian tal cual)
+
+//Copiar
+$tipos=['*.php','*.css','*.html','*.jpg','*.png'];
+copiar(_fuente,$tipos,_desarrollo,false);
+copiar(_fuente.'recursos/',$tipos,_desarrollo.'recursos/');
+copiar(_fuente.'servidor/',$tipos,_desarrollo.'servidor/');
+copiar(_fuente.'temp/',null,_desarrollo.'temp/');
+copiar(_fuente.'editor/',$tipos,_desarrollo.'editor/');
+copiar(_fuente.'editor/img/',null,_desarrollo.'editor/img/');
+
+//Comprimir css
+//No podemos buscar directamente en /desarrollo/ ya que debemos evitar tocar los css de la aplicación
+buscarArchivos(_desarrollo.'recursos/','*.css','comprimirCss');
+buscarArchivos(_desarrollo.'editor/','*.css','comprimirCss');
+
+////Librerías de terceros (se compian tal cual)
+//TOOD
+
+////js cliente (framework + componentes)
+
+//Compilar archivo combinado
+$archivos=[
+    'util.js',
+    'dom.js',
+    'arrastra.js',
+    'editable.js',
+    'servidor.js',
+    'ajax.js',
+    'componente.js',
+    'controlador.js',
+    'ui.js',
+    'expresion.js',
+    'componentes/**.js'
+];
+$arg='';
+foreach($archivos as $arch) $arg.=' "'.escapeshellarg(_fuente.'cliente/'.$arch).'"';
+exec(_closure.' --js_output_file "'.escapeshellarg(_desarrollo.'cliente/foxtrot.js').'" '.$arg);
+
+//Editor
+exec(_closure.' --js_output_file "'.escapeshellarg(_desarrollo.'editor/editor.js').'" --js "'.escapeshellarg(_fuente.'editor/editor.js').'"');
---
+++ scripts/funciones.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright, 2020, Gabriel Quagliano. Bajo licencia Apache 2.0.
+ * 
+ * @author Gabriel Quagliano - gabriel.quagliano@gmail.com
+ * @version 1.0
+ */
+
+//Funciones útiles y comunes para los scripts de compilación
+//Este es un script de PRUEBA (eventualmente se combinará con el resto del código del IDE)
+
+function copiar($ruta,$filtro,$destino,$rec=true) {
+    if(is_array($filtro)) {
+        foreach($filtro as $f) copiar($ruta,$f,$destino,$rec);
+        return;
+    }
+
+    if(!$filtro) $filtro='*';
+
+    if(!file_exists($destino)) mkdir($destino);
+
+    $arr=glob($ruta.$filtro);
+
+    if($rec) $arr=array_merge($arr,glob($ruta.'*',GLOB_ONLYDIR));
+
+    foreach($arr as $archivo) {
+        $archivo=basename($archivo);
+        if($archivo=='.'||$archivo=='..') continue;
+        if(is_dir($ruta.$archivo)) {
+            if($rec) copiar($ruta.$archivo.'/',$filtro,$destino.$archivo.'/',true);
+        } else {
+            copy($ruta.$archivo,$destino.$archivo);
+        }
+    }
+}
+
+function buscarArchivos($ruta,$filtro,$funcion) {
+    $arr=glob($ruta.$filtro);
+    $arr=array_merge($arr,glob($ruta.'*',GLOB_ONLYDIR));
+    foreach($arr as $archivo) {
+        $archivo=basename($archivo);
+        if($archivo=='.'||$archivo=='..') continue;
+        if(is_dir($ruta.$archivo)) {
+            buscarArchivos($ruta.$archivo.'/',$filtro,$funcion);
+        } else {
+            call_user_func($funcion,$ruta.$archivo);
+        }
+    }
+}
+
+function comprimirCss($archivo) {
+    $css=file_get_contents($archivo);
+
+    //Compresión rápida (solo limpieza de contenido innecesario)
+    $css=preg_replace('#/\*.*?\*/#sm','',$css);    
+    $css=preg_replace('#url\s*?\(\s*?(\'|")(.+?)\1\)#msi','url($2)',$css);
+    $css=str_replace(["\r","\n"],'',$css);
+    $css=preg_replace('/([\):;\}\{])[\s]+/m','$1',$css);
+    $css=preg_replace('/[\s]+([\{\(#])/m','$1',$css);
+    $css=str_replace(';}','}',$css);
+
+    file_put_contents($archivo,$css);
+}

40411fa074f7a31b7bab92cf95a5fb3484ea5f83
Gabriel Quagliano <gabriel.quagliano@gmail.com>
2020-08-23 21:45:30 -0300

    Implementado el componente Bucle 🙂.

--- fuente/cliente/componentes/bucle.js
+++ fuente/cliente/componentes/bucle.js
@@ -13,11 +13,27 @@
 var componenteBucle=function() {    
     this.componente="bucle";
 
+    this.itemsAutogenerados=[];
+
+    /**
+     * Propiedades de Bucle.
+     */
+    this.propiedadesConcretas={
+        "Buble":{
+            mensajeVacio:{
+                etiqueta:"Mensaje de bloque vacío",
+                ayuda:"Cuando se asigne un origen de datos vacío, se mostrará este mensaje.",
+                adaptativa:false
+            }
+        }
+    };
+
     /**
      * Inicializa la instancia tras ser creada o restaurada.
      */
     this.inicializar=function() {
         if(this.fueInicializado) return this; 
+        this.contenedor=this.elemento.querySelector(".componente-bucle-plantilla");
         this.inicializarComponente();
         return this;
     };
@@ -26,9 +42,154 @@ var componenteBucle=function() {
      * Crea el elemento del DOM para esta instancia (método para sobreescribir).
      */
     this.crear=function() {
-        this.elemento=document.crear(""); 
+        this.elemento=document.crear("<div><div class='componente-bucle-plantilla'></div></div>"); 
         this.crearComponente();
         return this;
+    };
+    
+    /**
+     * Establece el origen de datos. El mismo será aplicado a toda la descendencia en forma recursiva.
+     * @param {Object} obj - Objeto a asignar.
+     * @param {boolean} [actualizar=true] - Actualizar el componente luego de establecer el origen de datos.
+     * @returns Componente
+     */
+    this.establecerDatos=function(obj,actualizar) {        
+        if(typeof actualizar==="undefined") actualizar=true;
+
+        this.datos=obj;
+
+        if(actualizar) this.actualizar();
+
+        //No incovamos establecerDatosComponente ya que no queremos que el objeto se distribuya a la descendencia
+        return this;        
+    };
+
+    /**
+     * Actualiza el componente.
+     * @returns {Componente}
+     */
+    this.actualizar=function() {
+        //Limpiar filas autogeneradas
+        this.itemsAutogenerados.forEach(function(item) {
+            item.eliminar();
+        });
+        this.itemsAutogenerados=[];
+
+        if(!this.datos) return this;
+
+        //Ocultamos toda la descendencia para que las instancias originales de los campos que se van a duplicar no se vean afectadas al obtener/establecer los valores de la vista
+        this.ocultarDescendencia();
+
+        if(!this.datos.length) {
+            this.mostrarMensajeSinDatos();
+        } else {
+            this.generarItems();
+        }
+
+        return this;
+    };
+
+    /**
+     * Genera el mensaje de bloque sin datos.
+     * @returns {Componente}
+     */
+    this.mostrarMensajeSinDatos=function() {
+        var texto=this.propiedad(null,"mensajeVacio")
+            div=document.crear("tr");
+
+        div.agregarClase("autogenerado item-sin-datos")
+            .establecerHtml(texto);
+
+        this.contenedor.anexar(div);
+
+        return this;
+    };
+
+    /**
+     * Genera y devuelve un nuevo item.
+     * @param {Componente} elemento - Elemento a clonar.
+     * @param {Object} obj - Objeto a representar (datos del item).
+     * @param {number} indice - Indice del origen de datos (índice del elemento).
+     * @returns {Componente}
+     */
+    this.generarItem=function(hijo,obj,indice) {
+        var nuevo=hijo.clonar(this.elemento.padre(),true); //Anexar al padre del componente bucle
+
+        this.itemsAutogenerados.push(nuevo);
+
+        nuevo.establecerDatos(obj);
+        nuevo.indice=indice;
+        nuevo.autogenerado=true;
+        nuevo.obtenerElemento().agregarClase("autogenerado");
+
+        return nuevo;
+    };
+
+    /**
+     * Genera los items del componente.
+     * @returns {Componente}
+     */
+    this.generarItems=function() {
+        var t=this;
+
+        this.datos.forEach(function(obj,indice) {
+            t.obtenerHijos().forEach(function(hijo) {
+                if(!t.autogenerado) t.generarItem(hijo,obj,indice);
+            });
+        });
+
+        return this;
+    };
+
+    /**
+     * Devuelve o establece el valor del componente.
+     * @param {*} [valor] - Valor a establecer
+     * @returns {*}
+     */
+    this.valor=function(valor) {
+        if(typeof valor==="undefined") {
+            //Cuando se solicite el valor del componente, devolver el origen de datos actualizado con las propiedades que puedan haber cambiado
+            return this.obtenerDatosActualizados();            
+        } else {
+            //Cuando se asigne un valor, establecer como origen de datos
+            this.establecerDatos(valor);
+        }
+    };
+
+    /**
+     * Obtiene una copia del origen de datos actualizado con las propiedades que hayan cambiado por tratarse de componentes de ingreso de datos (campos, etc.)
+     * @returns {(Object[])}
+     */
+    this.obtenerDatosActualizados=function() {
+        var resultado=this.datos?this.datos.clonar():[];
+
+        var fn=function(comp,indice) {
+            comp.obtenerHijos().forEach(function(hijo) {
+                var propiedad=hijo.propiedad(null,"propiedad"),
+                    valor=hijo.valor();
+                if(propiedad&&typeof valor!=="undefined") resultado[indice][propiedad]=valor;
+                fn(hijo,indice);
+            });
+        };
+
+        this.obtenerHijos().forEach(function(hijo) {
+            if(!hijo.autogenerado) return;
+            if(resultado.length<=hijo.indice) resultado[hijo.indice]={};
+            
+            //Dentro de cada item, buscar recursivamente todos los componentes relacionados con una propiedad
+            fn(hijo,hijo.indice);
+        });
+
+        return resultado;
+    };
+
+    /**
+     * Busca todos los componentes con nombre que desciendan de este componente y devuelve un objeto con sus valores.
+     * @returns {Object}
+     */
+    this.obtenerValores=function() {
+        return;
+        //No queremos que continúe la búsqueda en forma recursiva entre los componentes autogenerados
     };
 };
 

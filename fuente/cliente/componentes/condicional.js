2a3a94a95a9fc889414b26fbaf8971ef90df3ece
Gabriel Quagliano <gabriel.quagliano@gmail.com>
2020-09-12 18:36:39 -0300

    Implementado el componente Condicional 🙌.

--- fuente/cliente/componentes/condicional.js
+++ fuente/cliente/componentes/condicional.js
@@ -14,10 +14,23 @@ var componenteCondicional=function() {
     this.componente="condicional";
 
     /**
+     * Propiedades de Condicional.
+     */
+    this.propiedadesConcretas={
+        "Condicional":{
+            condicion:{
+                etiqueta:"Condición",
+                adaptativa:false
+            }
+        }
+    };
+
+    /**
      * Inicializa la instancia tras ser creada o restaurada.
      */
     this.inicializar=function() {
         if(this.fueInicializado) return this; 
+        this.contenedor=this.elemento;
         this.inicializarComponente();
         return this;
     };
@@ -26,8 +39,42 @@ var componenteCondicional=function() {
      * Crea el elemento del DOM para esta instancia (método para sobreescribir).
      */
     this.crear=function() {
-        this.elemento=document.crear(""); 
+        this.elemento=document.crear("<div>"); 
+        this.contenedor=this.elemento;
         this.crearComponente();
+        return this;
+    };
+
+    /**
+     * Actualiza el componente.
+     */
+    this.actualizar=function() {
+        var condicion=this.procesarEvento("condicion","condicion");
+        if(condicion) {
+            this.elemento.agregarClase("visible");
+        } else {
+            this.elemento.removerClase("visible");
+        }
+        return this;
+    };
+
+    /**
+     * Evento Listo.
+     */
+    this.listoComponente=function() {
+        this.actualizar();
+        this.listoComponente();
+    };
+
+    /**
+     * Establece el valor del componente.
+     * @param {*} [valor] - Valor a establecer.
+     * @returns {(null|componente)}
+     */
+    this.valor=function(valor) {
+        if(typeof valor==="undefined") return this.datos;
+        //Cuando se asigne un valor, establecer como origen de datos
+        this.establecerDatos(valor);
         return this;
     };
 };
@@ -36,6 +83,5 @@ ui.registrarComponente("condicional",componenteCondicional,configComponente.clon
     descripcion:"Condicional",
     etiqueta:"Condicional",
     grupo:"Control",
-    icono:"condicional.png",
-    ocultar:true
+    icono:"condicional.png"
 }));

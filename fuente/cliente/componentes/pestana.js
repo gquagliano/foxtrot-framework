186dea3f6baba00bd183191bc80b83941d5f2c3d
Gabriel Quagliano <gabriel.quagliano@gmail.com>
2020-09-13 18:20:44 -0300

    Implementado el componente Pestaña 🙌.

--- fuente/cliente/componentes/pestana.js
+++ fuente/cliente/componentes/pestana.js
@@ -9,15 +9,34 @@
 
 /**
  * @class Componente concreto Pestaña.
+ * @extends {Componente}
  */
 var componentePestana=function() {    
     this.componente="pestana";
+
+    this.activa=false;
+
+    /**
+     * Propiedades de Pestaña.
+     */
+    this.propiedadesConcretas={
+        "Pestaña":{
+            etiqueta:{
+                etiqueta:"Etiqueta",
+                adaptativa:false
+            }
+        }
+    };    
 
     /**
      * Inicializa la instancia tras ser creada o restaurada.
      */
     this.inicializar=function() {
         if(this.fueInicializado) return this; 
+        this.contenedor=this.elemento;
+
+        if(this.elemento.es({ clase:"activa" })) this.activa=true;
+
         this.inicializarComponente();
         return this;
     };
@@ -26,9 +45,76 @@ var componentePestana=function() {
      * Crea el elemento del DOM para esta instancia (método para sobreescribir).
      */
     this.crear=function() {
-        this.elemento=document.crear(""); 
+        this.elemento=document.crear("<div>"); 
+        this.contenedor=this.elemento;
         this.crearComponente();
         return this;
+    };
+
+    /**
+     * Evento 'Insertado' (componente creado o movido en modo de edición).
+     */
+    this.insertado=function() {
+        this.actualizarContenedor();
+    };
+    
+    /**
+     * Actualiza el componente tras la modificación de una propiedad.
+     */
+    this.propiedadModificada=function(propiedad,valor,tamano,valorAnterior) {
+        if(propiedad=="etiqueta") this.actualizarContenedor();
+
+        this.propiedadModificadaComponente(propiedad,valor,tamano,valorAnterior);
+        return this;
+    };
+
+    /**
+     * Actualiza el contenedor de pestañas (componente Pestañas).
+     * @returns {componentePestana}
+     */
+    this.actualizarContenedor=function() {
+        var padre=this.obtenerPadre();
+        if(padre) padre.actualizarEncabezados();
+        return this;
+    };
+
+    /**
+     * Activa, o trae al frente, la pestaña.
+     * @returns {componentePestana}
+     */
+    this.activar=function() {
+        var padre=this.obtenerPadre();
+
+        padre.desactivarTodas();
+
+        this.elemento.agregarClase("activa");
+        ui.animarAparecer(this.elemento);
+        this.activa=true;
+
+        padre.actualizarEncabezados(false);
+
+        //Evento
+        padre.pestanaActivada(this);
+
+        return this;
+    };
+
+    /**
+     * Desactiva la pestaña.
+     * @returns {componentePestana}
+     */
+    this.desactivar=function() {
+        this.activa=false;
+        this.elemento.removerClase("activa");
+        return this;
+    };
+
+    /**
+     * Devuelve si la pestaña es la pestaña activa o no.
+     * @returns {boolean}
+     */
+    this.esActiva=function() {
+        return this.activa;
     };
 };
 
@@ -37,5 +123,5 @@ ui.registrarComponente("pestana",componentePestana,configComponente.clonar({
     etiqueta:"Pestaña",
     grupo:"Estructura",
     icono:"pestana.png",
-    ocultar:true
+    padre:["pestanas"]
 }));
--- fuente/cliente/componentes/pestanas.js
+++ fuente/cliente/componentes/pestanas.js
@@ -9,15 +9,36 @@
 
 /**
  * @class Componente concreto Bloque de pestañas.
+ * @extends {Componente}
  */
 var componentePestanas=function() {    
     this.componente="pestanas";
 
     /**
+     * Propiedades de Pestañas.
+     */
+    this.propiedadesConcretas={
+        "Eventos":{
+            pestanaActivada:{
+                etiqueta:"Pestaña activada",
+                evento:true,
+                adaptativa:false
+            }
+        }
+    };
+
+    this.encabezado=null;
+
+    /**
      * Inicializa la instancia tras ser creada o restaurada.
      */
     this.inicializar=function() {
-        if(this.fueInicializado) return this; 
+        if(this.fueInicializado) return this;
+
+        //TODO Desarrollar una forma más simple de buscar entre los hijos de un elemento
+        this.encabezado=this.elemento.childNodes.filtrar({ clase:"encabezados" })[0];
+        this.contenedor=this.elemento.childNodes.filtrar({ clase:"contenedor" })[0];
+
         this.inicializarComponente();
         return this;
     };
@@ -26,8 +47,105 @@ var componentePestanas=function() {
      * Crea el elemento del DOM para esta instancia (método para sobreescribir).
      */
     this.crear=function() {
-        this.elemento=document.crear(""); 
+        this.elemento=document.crear("<div>"); 
+
+        this.encabezado=document.crear("<div class='encabezados'>");
+        this.elemento.anexar(this.encabezado);
+
+        this.contenedor=document.crear("<div class='contenedor'>");
+        this.elemento.anexar(this.contenedor);
+
         this.crearComponente();
+        return this;
+    };
+    
+    /**
+     * Evento 'Listo'.
+     */
+    this.listo=function() {
+        this.actualizarEncabezados(false);
+    };   
+
+    /**
+     * Regenera los botones del encabezado.
+     * @param {boolean} [regenerar=true] - Regenerar botones.
+     * @returns {componentePestanas}
+     */
+    this.actualizarEncabezados=function(regenerar) {
+        if(typeof regenerar==="undefined") regenerar=true;
+
+        if(regenerar) this.encabezado.establecerHtml("");
+
+        var pestanaActiva=null;
+
+        var t=this;
+        this.obtenerHijos().forEach(function(pestana,i) {
+            var etiqueta=pestana.propiedad("etiqueta");
+            if(!etiqueta) etiqueta="Pestaña "+(i+1);
+
+            var boton;
+            
+            if(regenerar) {
+                boton=documento.crear("<button class='btn btn-etiqueta-pestana' data-indice='"+i+"'>");
+                boton.anexarA(t.encabezado);
+            } else {
+                boton=t.encabezado.querySelector("button:nth-child("+(i+1)+")");
+            }
+
+            boton.establecerHtml(etiqueta)
+                .removerEventos()
+                .evento("click",function(ev) {
+                    ev.preventDefault();
+                    ev.stopPropagation();
+                    t.activarPestana(parseInt(this.dato("indice")));
+                });
+
+            if(pestana.esActiva()) {
+                boton.agregarClase("activa");
+                pestanaActiva=i;
+            } else {
+                boton.removerClase("activa");
+            }
+        });
+
+        //Si ninguna pestaña está activa, activar la primera
+        if(pestanaActiva===null) this.activarPestana(0);
+
+        return this;
+    };
+
+    /**
+     * Desactiva todas las pestañas.
+     * @returns {componentePestanas}
+     */
+    this.desactivarTodas=function() {
+        this.obtenerHijos().forEach(function(pestana) {
+            pestana.desactivar();
+        });
+        return this;
+    };
+
+    /**
+     * Activa una pestaña dado su orden o instancia.
+     * @param {(number|Componente)} pestana - Pestaña a activar.
+     * @returns {componentePestanas}
+     */
+    this.activarPestana=function(pestana) {
+        if(typeof pestana==="number") {
+            var hijos=this.obtenerHijos();
+            if(hijos.length>pestana) pestana=hijos[pestana];
+        }
+        if(typeof pestana==="object") pestana.activar();
+        return this;
+    };
+
+    /**
+     * Ejecuta el evento pestanaActivada.
+     * @param {componentePestana} pestana - Pestaña activada.
+     * @returns {componentePestanas}
+     */
+    this.pestanaActivada=function(pestana) {
+        if(!ui.enModoEdicion()) this.procesarEvento("pestanaActivada","pestanaActivada",null,null,{pestana:pestana});
         return this;
     };
 };
@@ -37,5 +155,5 @@ ui.registrarComponente("pestanas",componentePestanas,configComponente.clonar({
     etiqueta:"Pestañas",
     grupo:"Estructura",
     icono:"pestanas.png",
-    ocultar:true
+    aceptaHijos:["pestana"]
 }));

c8dfdcb9c1c8c6b78fc679c31abc39642051c746
Gabriel Quagliano <gabriel.quagliano@gmail.com>
2020-09-14 23:00:00 -0300

    Corregide la ruta de phpdoc / jsdoc.

--- scripts/documentacion.bat
+++ scripts/documentacion.bat
@@ -13,7 +13,7 @@ if "%2"=="php" (set php=true)
 if "%1"=="js" (set js=true)
 if "%2"=="js" (set js=true)
 
-if %php%==true (php ../../phpDocumentor-2.9.1/bin/phpdoc.php -d "../fuente/servidor" -t "../documentacion/phpdoc")
-if %js%==true (jsdoc -r "../fuente/cliente" -d "../documentacion/jsdoc")
+if %php%==true (php ../../phpDocumentor-2.9.1/bin/phpdoc.php -d "../fuente/servidor" -t "../docs/phpdoc")
+if %js%==true (jsdoc -r "../fuente/cliente" -d "../docs/jsdoc")
 
     

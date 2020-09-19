c533efec7ca74018d931922c7ce2368a89d876d9
Gabriel Quagliano <gabriel.quagliano@gmail.com>
2020-09-19 17:31:23 -0300

    Script para ejecutar una vista embebible (Cordova) en en navegador.

---
+++ fuente/simular-cordova.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright, 2020, Gabriel Quagliano. Bajo licencia Apache 2.0.
+ * 
+ * @author Gabriel Quagliano - gabriel.quagliano@gmail.com
+ * @version 1.0
+ */
+
+define('_inc',1);
+
+include(__DIR__.'/servidor/foxtrot.php');
+foxtrot::inicializar(false);
+foxtrot::cargarAplicacion();
+
+//Intentar configurar index-cordova.html
+
+$html=file_get_contents(__DIR__.'/index-cordova.html');
+
+echo preg_replace([
+    '/_nombreApl=".+?"/',
+    '/_vistaInicial=".+?"/',
+],[
+    '_nombreApl="'.foxtrot::obtenerNombreAplicacion().'"',
+    '_vistaInicial="'.$_GET['vista'].'.html"'
+],$html);

6a563eb55c5a371301518a26f3310054c5ff71d3
Gabriel <gabriel.quagliano@gmail.com>
2020-10-29 22:12:02 -0300

    Agregada la posibilidad de enviar la respuesta tal cual. Documentación.

--- fuente/servidor/cliente.php
+++ fuente/servidor/cliente.php
@@ -14,17 +14,28 @@ defined('_inc') or exit;
 class cliente {
     protected $controlador;
     protected $esAplicacion=false;
+    protected static $responderCrudo=false;
 
     /**
-     * 
+     * Constructor.
+     * @param string $controlador Nombre del controlador.
      */
     function __construct($controlador=null) {
         $this->controlador=$controlador;
     }
 
     /**
+     * Activa o desactiva en envío de la respuesta sin procesar, en formato JSON. Cuando esta opción está desactivada, la respuesta se enviará de acuerdo al protocolo
+     * de comunicación con el cliente de Foxtrot.
+     * @param bool $activar Activar o desactivar la opción.
+     */
+    public static function establecerResponderCrudo($activar=true) {
+        self::$responderCrudo=$activar;
+    }
+
+    /**
      * Establece que la instancia del cliente es para comunicación, por defecto, con el controlador de aplicación.
-     * @returns cliente
+     * @return \cliente
      */
     public function establecerAplicacion() {
         $this->esAplicacion=true;
@@ -33,25 +44,39 @@ class cliente {
 
     /**
      * Envía una respuesta como texto plano al cliente.
+     * @param mixed $data Datos a devolver.
      */
     public static function responder($data) {
         if(!\foxtrot::esCli()) {
-            echo json_encode([
-                'r'=>$data
-            ]);
-        } elseif(is_array($data)||is_object($data)) {
-            print_r($data);
+            //Web
+            if(self::$responderCrudo) {
+                //Crudo (raw)
+                echo json_encode($data);
+            } else {
+                //Cliente de Foxtrot
+                echo json_encode([
+                    'r'=>$data
+                ]);
+            }
         } else {
-            echo $data;
+            //Línea de comandos
+            if(is_array($data)||is_object($data)) {
+                print_r($data);
+            } else {
+                echo $data;
+            }
         }
         \foxtrot::detener();
     }
 
     /**
-     * 
+     * Invoca un método del lado del cliente (solo como respuesta a solicitudes desde el cliente de Foxtrot).
+     * @param string $controlador Nombre del controlador (cliente).
+     * @param string $metodo Nombre del método.
+     * @param array|object $args Argumentos.
      */
     public static function invocar($controlador,$metodo,$args=null) {
-            if(!\foxtrot::esCli()) echo json_encode([
+        if(!\foxtrot::esCli()) echo json_encode([
                 'c'=>$controlador,
                 'm'=>$metodo,
                 'p'=>$args
@@ -60,7 +85,7 @@ class cliente {
     }
 
     /**
-     * Invoca un método en el controlador JS de la aplicación.
+     * Invoca un método en el controlador JS de la aplicación (solo como respuesta a solicitudes desde el cliente de Foxtrot).
      * @param string $metodo Nombre del método.
      * @param mixed $args=null Argumentos.
      */
@@ -73,7 +98,8 @@ class cliente {
     }
 
     /**
-     * 
+     * Realiza un redireccionamiento del lado del cliente (solo como respuesta a solicitudes desde el cliente de Foxtrot).
+     * @param string $ruta Ruta o URL de destino.
      */
     public static function irA($ruta) {
         if(!\foxtrot::esCli()) echo json_encode([
@@ -83,7 +109,7 @@ class cliente {
     }
 
     /**
-     * Redirige los llamados a métodos hacia el cliente.
+     * Redirige los llamados a métodos no definidos hacia el cliente.
      * @param string nombre - Nombre del método.
      * @param array $args - Argumentos.
      */

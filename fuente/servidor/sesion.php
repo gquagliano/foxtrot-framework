333d22f40ed155dc7406d7b8ac839a078edbea7f
Gabriel <gabriel.quagliano@gmail.com>
2020-11-08 09:15:10 -0300

    Ajustes. Documentación.

--- fuente/servidor/sesion.php
+++ fuente/servidor/sesion.php
@@ -12,25 +12,27 @@ defined('_inc') or exit;
  * Gestión de la sesión.
  */
 class sesion {
+    //TODO JWT
+
 	const sv='login_';
     protected static $usuario=null;
     protected static $datosPublicos=null;
 
     /**
-     * 
+     * Inicializa la sesión.
      */
 	public static function inicializar() {
 		//session_cache_limiter('private');
 		session_cache_expire(60);
 		session_start();
-
-		self::verificarUsuario();
 	}
 	
     /**
-     * 
+     * Establece la sesión.
+     * @param mixed $usuario Datos de la sesión.
+     * @param mixed $datosPublicos Datos públicos de la sesión (el cliente podrá solicitarlos en forma directa al framework).
      */
-	public static function establecerUsuario($usuario,$datosPublicos) {
+	public static function establecerUsuario($usuario,$datosPublicos=null) {
         self::$usuario=$usuario;
         self::$datosPublicos=$datosPublicos;
         $_SESSION[self::sv.'usuario']=$usuario;
@@ -38,7 +40,7 @@ class sesion {
 	}
 
     /**
-     * 
+     * Termina la sesión.
      */
 	public static function cerrarSesion() {
         self::$usuario=null;
@@ -47,30 +49,34 @@ class sesion {
 	}
 
     /**
-     * 
+     * Devuelve los datos del usuario.
+     * @return mixed
      */
 	public static function obtenerUsuario() {
 		return self::$usuario;
 	}
 
     /**
-     * 
+     * Devuelve los datos del usuario.
+     * @return mixed
      */
 	public static function obtenerDatosPublicos() {
 		return self::$datosPublicos;
 	}
 
     /**
-     * 
+     * Verifica si hay una sesión iniciada con datos establecidos.
+     * @return bool
      */
 	public static function verificarUsuario() {
         self::$usuario=$_SESSION[self::sv.'usuario'];
         self::$datosPublicos=$_SESSION[self::sv.'usuarioPub'];
-        return self::$datosPublicos?true:false;
+        return self::$usuario?true:false;
     }
     
     /**
-     * 
+     * Responde a la solicitud HTTP por los datos de usuario (`__f`=`sesion`).
+     * @return mixed
      */
     public static function responderSolicitud() {
         cliente::responder(self::$datosPublicos);

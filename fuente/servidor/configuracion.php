79034f7f1b0868b35eabdc56f1e4f24d521b0c7a
Gabriel <gabriel.quagliano@gmail.com>
2020-11-08 15:45:22 -0300

    Valor predeterminado de zonaHorariaMinutos.

--- fuente/servidor/configuracion.php
+++ fuente/servidor/configuracion.php
@@ -18,7 +18,7 @@ class configuracion {
     public static $enrutador=null;
     public static $rutaBase='/';
     public static $zonaHoraria='America/Argentina/Buenos_Aires';
-    public static $zonaHorariaMinutos=-180;
+    public static $zonaHorariaMinutos=0;
     public static $servidorBd='127.0.0.1';
     public static $puertoBd=3306;
     public static $usuarioBd;

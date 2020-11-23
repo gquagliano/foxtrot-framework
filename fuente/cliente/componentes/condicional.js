5857deeab770c9b19ec096c9bfdc30375dcbd63e
Gabriel <gabriel.quagliano@gmail.com>
2020-11-23 18:35:08 -0300

    No devolver valor.

--- fuente/cliente/componentes/condicional.js
+++ fuente/cliente/componentes/condicional.js
@@ -97,12 +97,13 @@ var componenteCondicional=function() {
     };
 
     /**
-     * Establece el valor del componente.
+     * Establece el valor del componente. Nótese que este componente no tiene valor propio, solo se ofrece este método como sinónimo de `establecerDatos()`; invocarlo sin
+     * especificar `valor` devolverá `null`.
      * @param {*} [valor] - Valor a establecer.
      * @returns {(null|componente)}
      */
     this.valor=function(valor) {
-        if(typeof valor==="undefined") return this.datos;
+        if(typeof valor==="undefined") return null;
         
         //Cuando se asigne un valor, establecer como origen de datos
         this.establecerDatos(valor);

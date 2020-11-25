50e3b320c04a33ed594fcccc8bb747e366267de6
Gabriel <gabriel.quagliano@gmail.com>
2020-11-25 14:45:49 -0300

    Removido un console.log() perdido.

--- fuente/cliente/controlador.js
+++ fuente/cliente/controlador.js
@@ -227,7 +227,6 @@ var controlador=new function() {
      * @param {(string|null)} tamanoAnterior - Tamaño anterior (`'xl'`,`'lg'`,`'md'`,`'sm'`,`'xs'` o `null` si es la primer invocación al cargar la vista).
      */
     this.tamano=function(tamano,tamanoAnterior) {
-        console.log('w',tamano,tamanoAnterior)
     };
     
     /**
